cf0475ab4d242786e6847c17d8695b6190f456a3
R L <ryan@Rain.local>
2022-06-19 12:04:00 -0700

    fixed webview go back

--- AI_Recycle_Helper/HomeViewController.swift
+++ AI_Recycle_Helper/HomeViewController.swift
@@ -7,17 +7,31 @@
 
 import UIKit
 import WebKit
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController,WKUIDelegate {
 
     @IBOutlet weak var webPage: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         //let myURL = URL(string:"https://recyclebc.ca/category/education/")
         webPage.load(URLRequest(url:URL(string:"https://recyclebc.ca/category/education/")!))
+        webPage.uiDelegate = self
     }
     
 
+    @IBAction func goToPrevious(_ sender: Any) {
+        if (webPage.canGoBack){
+            webPage.goBack()
+        }
+    }
     
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+            if let frame = navigationAction.targetFrame,
+                frame.isMainFrame {
+                return nil
+            }
+            webView.load(navigationAction.request)
+            return nil
+        }
     
     /*
     // MARK: - Navigation

6e58d5e2b774bda76a7fecd46b75cdbbe85fb086
R L <ryan@Rain-2.local>
2022-09-18 12:01:01 -0700

    fix camera permission

---
+++ AI_Recycle_Helper/tmpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  tmpViewController.swift
+//  AI_Recycle_Helper
+//
+//  Created by R L on 2022-09-18.
+//
+
+import UIKit
+
+class tmpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
